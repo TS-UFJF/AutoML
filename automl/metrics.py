1105d72e6db97681fce30b01211e8bfc794df9ee
Lucas <lrpessamilio@gmail.com>
2021-08-02 16:55:44 -0300

    Implemented multioutput handling

--- automl/metrics.py
+++ automl/metrics.py
@@ -1,29 +1,49 @@
 import numpy as np
+import warnings
+
+
+def _process_multioutput(arr, multioutput):
+    if(multioutput == 'uniform_average'):
+        return np.mean(arr)
+    if(multioutput == 'raw_values'):
+        return arr
+    if(isinstance(multioutput, list) and not isinstance(multioutput, str)):
+        if((np.squeeze(np.array(multioutput)).shape[0] == len(arr)) and np.squeeze(np.array(multioutput)).ndim == 1):
+            np.average(arr, weights=np.squeeze(np.array(multioutput)))
+        else:
+            warnings.warn(f"Expected shape {arr.shape} from 'multioutput' and received shape {np.array(multioutput).shape}" +
+                          ", returning an 'uniform_average'")
+            return np.mean(arr)
+    else:
+        print("The value in multioutput is invalid, returning an 'uniform_average'")
+        return np.mean(arr)
+
 
 # When inplementing new metrics remember that AutoML passes a 2D array
 # with shape [instances, timesteps]
 
 
-def weighted_absolute_percentage_error(y_true, y_pred, average=True):
+def weighted_absolute_percentage_error(y_true, y_pred, multioutput='uniform_average'):
     """
     The Weighted Absolute Percentage Error (WAPE) metric measures the overall
     deviation of forecasted values from observed values.
 
     :param y_true: The observed values.
     :param y_pred: The predicted values.
-    :param average: If average is set to True, returns the average of all instances,
-        otherwise, returns an array of the WAPEs of all instances.
+    :param multioutput: can be {'raw_values', 'uniform_average'}, default='uniform_average'
+        Defines aggregating of multiple output values. Array-like value defines weights used to average errors.
+
+        'raw_values' :
+            Returns a full set of errors in case of multioutput input.
+        'uniform_average' :
+            Errors of all outputs are averaged with uniform weight.
 
     """
 
     absolute_error_sum = np.sum(np.abs(y_true - y_pred), axis=1)
 
     loss = absolute_error_sum / np.sum(np.abs(y_true), axis=1)
-
-    if(average):
-        return np.mean(loss)
-    else:
-        return loss
+    return _process_multioutput(loss, multioutput)
 
 
 def mean_absolute_percentage_error(y_true, y_pred, average=True):
