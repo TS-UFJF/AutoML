78cb441145d0477b01bb5ed2bd417fbfa6cee2d6
Lucas <lrpessamilio@gmail.com>
2021-08-02 17:04:39 -0300

    Adapted current metrics to _process_multioutput

--- automl/metrics.py
+++ automl/metrics.py
@@ -12,10 +12,11 @@ def _process_multioutput(arr, multioutput):
             np.average(arr, weights=np.squeeze(np.array(multioutput)))
         else:
             warnings.warn(f"Expected shape {arr.shape} from 'multioutput' and received shape {np.array(multioutput).shape}" +
-                          ", returning an 'uniform_average'")
+                          ", returning an 'uniform_average'", SyntaxWarning)
             return np.mean(arr)
     else:
-        print("The value in multioutput is invalid, returning an 'uniform_average'")
+        warnings.warn(
+            "The value in multioutput is invalid, returning an 'uniform_average'", SyntaxWarning)
         return np.mean(arr)
 
 
@@ -30,7 +31,7 @@ def weighted_absolute_percentage_error(y_true, y_pred, multioutput='uniform_aver
 
     :param y_true: The observed values.
     :param y_pred: The predicted values.
-    :param multioutput: can be {'raw_values', 'uniform_average'}, default='uniform_average'
+    :param multioutput: can be {'raw_values', 'uniform_average'} or array-like of shape (n_outputs,), default='uniform_average'
         Defines aggregating of multiple output values. Array-like value defines weights used to average errors.
 
         'raw_values' :
@@ -41,20 +42,16 @@ def weighted_absolute_percentage_error(y_true, y_pred, multioutput='uniform_aver
     """
 
     absolute_error_sum = np.sum(np.abs(y_true - y_pred), axis=1)
-
     loss = absolute_error_sum / np.sum(np.abs(y_true), axis=1)
     return _process_multioutput(loss, multioutput)
 
 
-def mean_absolute_percentage_error(y_true, y_pred, average=True):
+def mean_absolute_percentage_error(y_true, y_pred, multioutput='uniform_average'):
     mape = np.mean(np.abs((y_true - y_pred) / y_true), axis=1) * 100
-    if(average):
-        return np.mean(mape)
-    else:
-        return mape
+    return _process_multioutput(mape, multioutput)
 
 
-def root_relative_squared_error(y_true, y_pred, average=True):
+def root_relative_squared_error(y_true, y_pred, multioutput='uniform_average'):
     """
     Vectorized implementation of RSE
     """
@@ -62,7 +59,4 @@ def root_relative_squared_error(y_true, y_pred, average=True):
         np.mean(y_true, axis=0, keepdims=True).T, y_true.shape[0], axis=1)
     numerator = np.sum(np.power(y_pred-y_true, 2), axis=1)
     denominator = np.sum(y_true-average_y_true, axis=1)
-    if(average):
-        return np.mean(np.sqrt(numerator/denominator))
-    else:
-        return np.sqrt(numerator/denominator)
+    return _process_multioutput((numerator/denominator), multioutput)
